6f7650bd8a51ef56c974f50bc207e51f9a79e3b7
Anthony Bowyer-Lowe <ynohtna@ynohtna.org>
2021-04-13 12:46:32 +0100

    identified personal build in version text

--- src/core/chuck_version.h
+++ src/core/chuck_version.h
@@ -2,7 +2,7 @@
 #define __CHUCK_VERSION_H_
 
 #define CHUCK_BUILD_NUMBER "1.4.0.1"
-#define CHUCK_BUILD_NAME "numchucks"
+#define CHUCK_BUILD_NAME "numchucks:ynohtna"
 
 #define CHUCK_BUILD_VERSION CHUCK_BUILD_NUMBER " (" CHUCK_BUILD_NAME ")"
 
